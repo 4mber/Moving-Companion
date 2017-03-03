46e0a468b1e49df5c614f3924cd12d1fd47b7fa0
Amber Trask <amberleightrask@gmail.com>
2017-03-03 15:53:59 +0000

    Updated to HTTPS
    Changed HTTP to HTTPS on all 3 apis.

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ function loadData() {
 
     $greeting.text('So, you want to live at ' + address + '?');
 
-    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
     var nytimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=12f9828ac1164ca49bfcd3c7f8c151cc'
